b4fc4c501860591b5022edf4ee48cae30996cd06
Tinh <vuivaidan123@gmail.com>
2022-03-05 22:53:41 +0700

    Add Http Post-put-delete for productcontroller

--- Rookies.BackEnd/Controllers/ProductController.cs
+++ Rookies.BackEnd/Controllers/ProductController.cs
@@ -1,14 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Web;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Rookies.BackEnd.Data;
 using Rookies.BackEnd.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Rookies.ShareClassdLibrary;
 using Rookies.ShareClassdLibrary.Dto.Product;
 using Rookies.ShareClassdLibrary.Enum;
@@ -92,6 +90,57 @@ namespace Rookies.BackEnd.Controllers
             return productVM;
         }
 
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult<ProductDto>> PostProduct(string name, int price, string description)
+        {
+            var product = new Product
+            {
+                Name = name,
+                Price = price,
+                CreatedDate = DateTime.Now,
+                Description = description
+            };
+            _context.Product.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduct", new { id = product.Id },
+                new ProductDto { Name = product.Name, Price = product.Price
+                , CreatedDate = product.CreatedDate, Description = product.Description});
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutProduct(int id)
+        {
+            var product = await _context.Product.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _context.Product.Update(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            //_context.Brands.Remove(brand);
+            product.IsDeleted = true;
+            _context.Product.Update(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(true);
+        }
+
         private IQueryable<Product> ProductFilter(
            IQueryable<Product> productQuery,
            ProductCriteriaDto productCriteriaDto)
@@ -112,116 +161,5 @@ namespace Rookies.BackEnd.Controllers
 
             return productQuery;
         }
-
-        //    [HttpGet]
-        //    public async Task<IActionResult> GetProduct()
-        //    {
-        //        var product = await _context.Product.ToListAsync();
-
-        //        return View(product);
-        //    }
-        //    public async Task<IActionResult> Details(int? id)
-        //    {
-        //        var productdetail = await _context.Product
-        //            .FirstAsync(x => x.Id == id);
-
-        //        if (productdetail == null)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        return View(productdetail);
-        //    }
-
-
-        //    //[Authorize] Making code for admin
-        //    public async Task<IActionResult> Create(Product product)
-        //    {
-        //        _context.Add(product);
-        //        await _context.SaveChangesAsync();
-        //        return View();
-        //    }
-
-        //    [HttpGet]
-        //    public async Task<IActionResult> Edit(int? id)
-        //    {
-        //        if (id == null)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        var product = await _context.Product.FindAsync(id);
-        //        if (product == null)
-        //        {
-        //            return NotFound();
-        //        }
-        //        ViewData["CategoryId"] = new SelectList(_context.Set<Category>(), "Id", "Name", product.CategoryId);
-        //        return View(product);
-        //    }
-
-        //    [HttpPost]
-        //    [ValidateAntiForgeryToken]
-        //    public async Task<IActionResult> Edit(int id, Product product)
-        //    {
-        //        if (id != product.Id)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        if (ModelState.IsValid)
-        //        {
-        //            try
-        //            {
-        //                _context.Update(product);
-        //                await _context.SaveChangesAsync();
-        //            }
-        //            catch (DbUpdateConcurrencyException)
-        //            {
-        //                if (!ProductExists(product.Id))
-        //                {
-        //                    return NotFound();
-        //                }
-        //                else
-        //                {
-        //                    throw;
-        //                }
-        //            }
-        //            return RedirectToAction(nameof(Index));
-        //        }
-        //        ViewData["CategoryId"] = new SelectList(_context.Set<Category>(), "Id", "Id", product.CategoryId);
-        //        return View(product);
-        //    }
-        //    //[HttpGet]
-        //    public async Task<IActionResult> Delete(int? id)
-        //    {
-        //        if (id == null)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        var product = await _context.Product
-        //            .FirstOrDefaultAsync(m => m.Id == id);
-        //        if (product == null)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        return View(product);
-        //    }
-
-        //    [HttpPost, ActionName("Delete")]
-        //    [ValidateAntiForgeryToken]
-        //    public async Task<IActionResult> DeleteConfirmed(int id)
-        //    {
-        //        var product = await _context.Product.FindAsync(id);
-        //        _context.Product.Remove(product);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-
-        //    private bool ProductExists(int id)
-        //    {
-        //        return _context.Product.Any(e => e.Id == id);
-        //    }
     }
 }
